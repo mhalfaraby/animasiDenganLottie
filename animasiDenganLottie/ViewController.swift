fd7ac74d37782c08f0215ab2eb7ea4113a3ff176
mhalfaraby <mhalfaraby@gmail.com>
2020-10-22 18:13:25 +0700

    pindah folder

--- animasiDenganLottie/ViewController.swift
+++ animasiDenganLottie/ViewController.swift
@@ -6,12 +6,23 @@
 //
 
 import UIKit
-
+import Lottie
 class ViewController: UIViewController {
-
+    let animationView = AnimationView()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupAnimation()
+    }
+    
+    private func setupAnimation() {
+        animationView.animation = Animation.named("uploading")
+        animationView.backgroundColor = .white
+        animationView.frame = view.bounds
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+        view.addSubview(animationView)
     }
 
 
